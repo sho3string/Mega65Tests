db82f6a62575b1b212d536cae23d9a00b1ee3a08
Samuel <36328867+sho3string@users.noreply.github.com>
2026-01-31 20:00:46 +1100

    Add files via upload

---
+++ RRB_NCMMode/make.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set WRITE="G:\Mega65\Dev\bin\cc1541.exe" -attach "./bin/DISK.D81" 8 -write
+set FORMAT="G:\Mega65\Dev\bin\cc1541.exe" -format "disk,0" d81 "./bin/DISK.D81"
+set KICKASM=java -cp G:\Mega65\YiearKungFu\Bin\kickassembler-5.24-65ce02.e.jar  kickass.KickAssembler65CE02  -vicesymbols -showmem -symbolfile -bytedumpfile main.klist
+
+
+echo ASSEMBLING SOURCES...
+%KICKASM%  main.s
+
+echo CREATING DISK IMAGE AND BOOT PRG...
+
+"G:\Mega65\Xemu\xmega65.exe" -uartmon :4510 -besure  -prg "main.prg"
+
+
